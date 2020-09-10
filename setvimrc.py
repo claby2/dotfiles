9ff4861e4a093c225afee93ad92bd791f38c9a66
claby2 <wibow9770@gmail.com>
2020-09-10 18:23:54 +0800

    add setvimrc script

---
+++ setvimrc.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This script replaces the current vimrc to the vimrc in the same directory of this script"""
+import platform
+import shutil
+
+def main():
+    """Copy vimrc"""
+    if platform.system() == "Windows":
+        shutil.copyfile("vimrc", "C:/Users/wibow9770/_vimrc")
+    elif platform.system() == "Darwin":
+        shutil.copyfile("vimrc", "~/Users/wibow9770/.vimrc")
+
+if __name__ == "__main__":
+    main()
